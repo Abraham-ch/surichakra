94b9dbfc212a74f147951383fccbfc33b8f3cb9c
Abraham-ch <117390894+Abraham-ch@users.noreply.github.com>
2024-04-16 22:38:59 -0500

    Update NoPage.jsx

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,7 +1,17 @@
+import Header from "../components/Header";
+
 export default function NoPage() {
   return (
     <>
-      <div>Not found</div>
+      <div>
+        <Header />
+        <div className="flex flex-col gap-y-4 w-full h-[600px] items-center justify-center ">
+          <div className="text-9xl font-bold tracking-widest">404</div>
+          <div className="text-5xl font-bold tracking-tight">
+            Sitio no encontrado
+          </div>
+        </div>
+      </div>
     </>
   );
 }
